8e7f00eb5593ec9a60c60392b91814a0008b1254
brunocotto <bruno.ottoo@gmail.com>
2022-09-24 16:48:41 -0300

    rota de login criada

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,11 +1,25 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+require("dotenv").config({path:"./.env"})
+
+exports.checkUserID = async(req, res) => {
+    const id = req.params.id
+
+    //check if user exists
+    const user = await User.findById(id, '-password')
+
+    if(!user) {
+        res.status(404).json({ msg: 'User not a found.' });
+        return
+    }
+
+    res.status(200).json({ user });
+}
 
 //funcão registro de usuários
 exports.register = async (req, res) => {
-    
+    //desestructured
     const { name, email, password } = req.body;
 
     // validations
@@ -24,17 +38,15 @@ exports.register = async (req, res) => {
         return
     }
     //o user já existe?
-    const userExists = await User.findOne({ email: email })
-
-    if (userExists) {
-        res.status(422).json({ msg: 'Email exist.' });
+    if (await User.findOne({ email: email })) {
+        res.status(422).json({ msg: 'User already exists.' });
         return
     }
 
-    // create password
-    const salt = await bcrypt.genSalt(12)
+    // create hash password
+    const salt = await bcrypt.genSalt(12) 
     const passwordHash = await bcrypt.hash(password, salt)
-
+    
     // create user
     const user = new User({
         name,
@@ -46,7 +58,8 @@ exports.register = async (req, res) => {
     try {
         await user.save()
 
-        res.status(201).json({ message: 'User created successfully.' });
+        //res.status(201).json({ message: 'User created successfully.' });
+        res.send({ user });
     } catch (error) {
         console.log(error)
 
@@ -54,3 +67,48 @@ exports.register = async (req, res) => {
     }
 };
 
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    // validations
+    if (!email || !password) {
+        res.status(422).json({ error: 'Email or password is invalid.' });
+        return
+    }
+
+    // check if user exists envio o password apenas para checkar as if password:password
+    const user = await User.findOne({ email: email }).select('+password')
+
+    if (!user) {
+        res.status(422).json({ message: 'User not a found.' });
+        return
+    }
+
+    // check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if (!checkPassword) {
+        res.status(422).json({ error: 'Password invalid.' });
+        return
+    }
+
+    try {
+        const secret = process.env.SECRET;
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            secret,
+        )
+
+        res.status(200).json({ message: 'Authentication performed successfully.', token });
+    } catch (error) {
+        console.log(error)
+
+        res
+            .status(500)
+            .json({ msg: error })
+    }
+
+}
